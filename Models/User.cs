81f2bc946f0c9bab5665ef433b25891e42589f71
ViniHGV <ixxvinicius@gmail.com>
2024-01-02 19:16:36 -0300

    feat: redefinindo a model User

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace TaskHubAPI.Models
 {
@@ -8,9 +6,8 @@ namespace TaskHubAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public EmailAddressAttribute Email { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public DateTime DateCreateAccount { get; set; } = DateTime.Now;
-        public ICollection<Task> Tasks { get; set; }
     }
 }
