7ddedbf0683b2636106143fffb9036bf7013a6ad
ViniHGV <ixxvinicius@gmail.com>
2024-01-03 19:43:07 -0300

    feat: adicionando database da maquina

--- src/Context/AppDbContext.cs
+++ src/Context/AppDbContext.cs
@@ -10,6 +10,6 @@ namespace TaskHubAPI.Context
         
         protected override void OnConfiguring(
             DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=localhost; Database=TaskHub; Username=postgres; Password=123");
+            => optionsBuilder.UseNpgsql("Host=localhost; Database=mydb; Username=postgres; Password=Vv1n1C1u$2309");
     }
 }
