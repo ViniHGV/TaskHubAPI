cbd3ebf34bb94ca27e4850c10a9ef36253cd5105
ViniHGV <ixxvinicius@gmail.com>
2024-01-16 21:22:04 -0300

    feat: atualizando arquivos de configuração do projeto

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Text.Json.Serialization;
 
 namespace TaskHubAPI.Models
