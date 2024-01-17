748e63c496f550ae9ea8ee3712a7d0d2232e7e5d
ViniHGV <ixxvinicius@gmail.com>
2024-01-17 16:37:37 -0300

    feat: removendo usings não utilizados

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Text.Json.Serialization;
 
 namespace TaskHubAPI.Models
 {
