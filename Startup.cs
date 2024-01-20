19c1c1ae23588e6c81d6a67e5470ca7eeb5352b4
ViniHGV <ixxvinicius@gmail.com>
2024-01-19 22:09:17 -0300

    feat: adicionando swagger para documentação do projeto

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using TaskHubAPI.Context;
 using TaskHubAPI.Services.User;
 using TaskHubAPI.src.Services.Tasks;
@@ -12,12 +13,16 @@ namespace TaskHubAPI
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new OpenApiInfo{Title = "TaskHubAPI", Version = "v1"});
+            });
+
             services.AddControllers()
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
                 });
-                services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>();
             services.AddScoped<TaskService>();
             services.AddScoped<UserService>();
         }
@@ -27,6 +32,9 @@ namespace TaskHubAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
+                app.UseSwagger();
+                app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskHubAPI v1"));
             }
 
             app.UseRouting();
