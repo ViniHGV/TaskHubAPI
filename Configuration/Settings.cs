1f9d862ba6ecc206c72e029ffc341cc2012544e7
ViniHGV <ixxvinicius@gmail.com>
2024-01-21 22:40:09 -0300

    feat: criando Class de Settings para acesso a chave privada para geração de jwt

---
+++ Configuration/Settings.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
+namespace TaskHubAPI.Configuration
+{
+    public static class Settings
+    {
+        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+        public static string Issuer = "https://github.com/ViniHGV";
+        public static string Audience = "https://github.com/ViniHGV";
+    }
+}
